ba88abf446fad57868baa81c21ad7e2eab1aa5e9
eyeignite <eyeignite402@gmail.com>
2023-10-06 09:35:19 +0100

    create new function to create a new div element and append it to the container 16 times

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function baseGrid() {
   makeRows(16);
   makeColumns(16);
 }
+
+function makeRows(rowVal) {
+  for (let i = 0; i < rowVal; i++) {
+    let row = document.createElement('div');
+    container.appendChild(row).classList.add('gridRows');
+  }
+}
