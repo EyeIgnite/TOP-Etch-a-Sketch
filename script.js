f78265d1b59242aef0c2556e2bd4fb378f693ff2
eyeignite <eyeignite402@gmail.com>
2023-10-06 08:32:13 +0100

    create variables for grid and functions to generate rows

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const gridContainer = document.querySelector('#container');
+const container = document.querySelector('#container');
+let rows = document.getElementsByClassName('row');
+let columns = document.getElementsByClassName('column');
+
+function grid() {
+  makeRows(16);
+  makeColumns(16);
+}
+
+function makeRows(rowVal) {
+  for (let r = 0; r < rowVal; r++) {
+    let row = document.createElement('div');
+    container.appendChild(row).className('row');
+  }
+}
