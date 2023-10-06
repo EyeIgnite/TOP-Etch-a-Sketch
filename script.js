03074b3eb3a8fe3ef6014b45d95eeb9173673415
eyeignite <eyeignite402@gmail.com>
2023-10-06 08:47:19 +0100

    test new function methods for making columns

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ function makeRows(rowVal) {
   for (let r = 0; r < rowVal; r++) {
     let row = document.createElement('div');
     container.appendChild(row).className('row');
-  }
-}
+  };
+};
 
 function makeColumns(columnVal) {
-  for (let c = 0; c < columnVal; c++) {
-    let column = document.createElement('div');
-    container.appendChild(column).className('column');
-  }
-}
+  for (let i = 0; i < rows.length; i++) {
+    for (let c = 0; c < columnVal; c++) {
+      let column = document.createElement('div');
+      rows[c].appendChild(column).className('column');
+    };
+  };
+};
