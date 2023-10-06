4a85e9f53abfe47f93f0a85f952d9eed2b4b212a
eyeignite <eyeignite402@gmail.com>
2023-10-06 09:40:40 +0100

    create new function for column generation

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function baseGrid() {
 function makeRows(rowVal) {
   for (let i = 0; i < rowVal; i++) {
     let row = document.createElement('div');
-    container.appendChild(row).classList.add('gridRows');
+    container.appendChild(row).classList.add('grid-rows');
   }
 }
+
+function makeColumns(columnVal) {
+  for (let i = 0; i < columnVal; i++) {
+    let column = document.createElement('div');
+    container.appendChild(column).classList.add('grid-column');
+  }
+}
+
+baseGrid();
