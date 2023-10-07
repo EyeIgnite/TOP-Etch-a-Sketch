b8b1dbc2ea18c09781bcc443950623e82af78b15
eyeignite <eyeignite402@gmail.com>
2023-10-07 20:35:50 +0100

    add comments to js for future reference

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const rows = document.getElementsByClassName('sketch-columns');
 const gridContainer = document.querySelector('.grid-container');
 
+// Creates default 16x16 grid
 function baseGrid(column, cell) {
   makeRows(column);
   makeColumns(cell);
 };
 
 baseGrid(16, 16);
-
+// Switches color to black when you mouseover each div
 const item = document.querySelectorAll('.cells');
 item.forEach((item) => {
   item.addEventListener('mouseover', () => {
@@ -15,14 +16,13 @@ item.forEach((item) => {
   });
 });
 
-
+// Functions used in baseGrid() to create the default
 function makeRows(columnVal) {
   for (let i = 0; i < columnVal; i++) {
-    let row = document.createElement('div');
-    gridContainer.appendChild(row).classList.add('sketch-columns');
+    let column = document.createElement('div');
+    gridContainer.appendChild(column).classList.add('sketch-columns');
   };
 };
-
 function makeColumns(cellVal) {
   for (let i = 0; i < rows.length; i++) {
     for (let c = 0; c < cellVal; c++) {
@@ -31,20 +31,20 @@ function makeColumns(cellVal) {
     };
   };
 };
-
+// Button event to clear the current grid
 const btn = document.querySelector('#clear');
 btn.addEventListener('click', () => {
   let input;
-
+  // Repeats until a number between 1-100 typed in
   while(true) {
-    input = prompt('Enter a number up to 100 for a new grid!');
-
+    input = prompt('Enter a number from 1-100 to create a new grid with those dimensions!');
+    // Exits function is user hits cancel on the prompt
     if (input === null) {
       return;
     }
 
     const numberInput = parseInt(input);
-
+    // checks for input to be in the correct range
     if (numberInput >= 1 && numberInput <= 100) {
     break; 
     }
@@ -52,13 +52,13 @@ btn.addEventListener('click', () => {
       alert('Please enter a valid number from 1 to 100');
     }
   };
-
+  // Checks the length of gridContainer's children and removes them until none are left
   while (gridContainer.childNodes.length > 0) {
     gridContainer.removeChild(gridContainer.lastChild);
   }
 
   baseGrid(input, input);
-  
+  // Adds mouseoever event to the newly created grid
   const item = document.querySelectorAll('.cells');
   item.forEach((item) => {
     item.addEventListener('mouseover', () => {
@@ -69,3 +69,4 @@ btn.addEventListener('click', () => {
 
 
 
+// Eraser is easy enough. Just add a click event to the button that changes each item to white
